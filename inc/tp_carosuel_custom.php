15b0f18a54cf72c85d10ae3a41fd26e5bc0301d3
yash007 <yash007@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-08 05:12:08 +0000

    more filed added
    git-svn-id: https://plugins.svn.wordpress.org/wp-smart-carousel/trunk@1176453 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/tp_carosuel_custom.php
@@ -0,0 +1,40 @@
+<?php
+
+// register custom post
+add_action( 'init', 'tp_carousel_post_type' );
+function tp_carousel_post_type() {
+	register_post_type( 'tp-carousel-items',
+		array(
+			'labels' => array(
+				'name' => __( 'Smart Carousels' ),
+				'singular_name' => __( 'Smart Carousel' ),
+				'add_new_item' => __( 'Add New Service' )
+			),
+			'public' => true,
+			'supports' => array('thumbnail', 'title', 'custom-fields', 'editor'),
+			'has_archive' => true,
+			'rewrite' => array('slug' => 'carousel-items'),
+		)
+	);	
+}
+
+//Register Taxonomy for custom post.
+function tp_carousel_taxonomy() {
+	register_taxonomy(
+		'tpcarousel_cat',  //The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces).
+		'tp-carousel-items',                  // ur custom post type name
+		array(
+			'hierarchical'          => true,
+			'label'                 => 'Carousel Category',  //Display name
+			'query_var'             => true,
+			'hierarchical'     	 	=> true,
+			'show_ui'          	 	=> true,
+			'show_admin_column' 	=> true,
+			'rewrite'               => array(
+				'slug'              => 'carousel-category', // This controls the base slug that will display before each term
+				'with_front'    => true // Don't display the category base before
+				)
+			)
+	);
+}
+add_action( 'init', 'tp_carousel_taxonomy');  
